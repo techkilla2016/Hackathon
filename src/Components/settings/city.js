cd1b1012ef7ce4d3faae1d4eecea7ac4eea0bf18
w3App <kshatriyakabawat@gmail.com>
2023-09-12 19:58:19 +0530

    Download data

--- src/Components/settings/city.js
+++ src/Components/settings/city.js
@@ -3,11 +3,13 @@ import { MdOutlineModeEditOutline } from 'react-icons/md'
 import { db } from '../../firebaseConfig';
 import { collection, addDoc, setDoc, getDoc, doc, getDocs, onSnapshot } from "firebase/firestore";
 import { AiOutlineSearch } from 'react-icons/ai'
+import { exportToExcel } from 'react-json-to-excel';
+
 var subscribe = undefined;
 
 const CityTable = ({ city, building }) => {
 
-    const [searchVal, setSearchVal] = useState('')
+
     const [cityData, setCityData] = useState([]);
 
     useEffect(() => {
@@ -16,40 +18,30 @@ const CityTable = ({ city, building }) => {
                 var docs = snapshot.docs;
                 setCityData(docs)
             });
-    }, [!subscribe])
+    }, [!subscribe]
+    )
 
-    const filter = (e) => {
-        cityData.filter((doc) => {
-            return false
+    const filter = (e)=>{
+        cityData.filter((doc)=>{
+           return false
         })
     }
 
-    const handlChange = ({ target }) => {
-        setSearchVal(target.value)
-        if (target.value == '') {
-            subscribe = onSnapshot(collection(db, "Users"),
-                (snapshot) => {
-                    var docs = snapshot.docs;
-                    setCityData(docs)
-                });
-        }
-    }
-    const handleSearch = () => {
-        const newCity = cityData.filter((item) => {
-            const val = item.data().obj?.Email
-            if (val.includes(searchVal)) {
-                return item
-            }
-        })
-        setCityData(newCity)
+    var array = [];
+
+    const download = ()=>{
+        var date = new Date();
+       let fname =  building.toLowerCase() + "_" + date.getDate().toString() + "-" + (date.getMonth() +1).toString() + "-" + date.getFullYear().toString() + "_" + date.getHours().toString()+"-"+date.getMinutes().toString()+"-"+date.getSeconds().toString();
+
+        exportToExcel(array, fname)
     }
 
     return (
         <div className='setting py-4'>
             <div className="search d-flex justify-content-start py-4">
-                <input onKeyUp={filter} type="search" name="" id="search" onChange={handlChange} value={searchVal} placeholder='Enter email id' className='form-control py-2' />
-                <button className='btn btn-primary mx-2' onClick={handleSearch}><AiOutlineSearch /></button>
-                <button className='btn btn-primary mx-4'>Download</button>
+                <input onKeyUp={filter} type="search" name="" id="search" placeholder='Enter email id' className='form-control py-2' />
+                <button className='btn btn-primary mx-2'><AiOutlineSearch /></button>
+                <button onClick={download} className='btn btn-primary mx-4'>Download</button>
             </div>
             <div className="setting-list">
                 <table>
@@ -67,12 +59,18 @@ const CityTable = ({ city, building }) => {
                     </thead>
                     <tbody>
                         {
+                        
                             cityData?.map((curItem, keys) => {
                                 // console.log(building.toLowerCase() === curItem.data().obj.building,city == curItem.data().obj.City)
                                 // console.log(building.toLowerCase(), curItem.data().obj.building, city, curItem.data().obj.City)
                                 if ((building.toLowerCase() === curItem.data().obj?.building) || city == "all") {
+                                    let newObj = {...curItem.data().obj};
+                                    delete newObj["Sno"];
+
+                                    array.push(newObj);
                                     return (
 
+                                      
                                         <tr key={keys} className={`${keys % 2 !== 0 && 'row-1'}`}>
                                             <td>{curItem.data().obj?.Name}</td>
                                             <td>{curItem.data().obj?.EmailTime}</td>
@@ -89,8 +87,15 @@ const CityTable = ({ city, building }) => {
                                             </td> */}
                                         </tr>)
                                 }
+
+                               
+                               
                             })
+
+                         
+                           
                         }
+                           
 
                     </tbody>
                 </table>
